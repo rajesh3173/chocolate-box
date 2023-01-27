42363d8cd22c5137b1fb881c6c43224ab0fd170f
Rajesh Kumar_ Ruttala (rarajeshkumar@deloitte.com) <rarajeshkumar@your.hostname.com>
2023-01-27 18:07:11 +0530

    Created error popup with certain message (document picking error, document reading error, data storing error)

--- Components/CustomButton.js
+++ Components/CustomButton.js
@@ -2,13 +2,13 @@ import { Pressable, StyleSheet, Text, View } from "react-native";
 import Colors from "../Constants/colors";
 import { scale } from "../Context/scales";
 
-const CustomButton = ({ children, onSelect }) => {
+const CustomButton = ({ children, onSelect, status }) => {
 
     return (
-        <View style={[styles.container, children == "CONTINUE" ? { backgroundColor: Colors.secondary900 } : { backgroundColor: Colors.secondary500 }]}>
+        <View style={[styles.container, status == "ok" ? { backgroundColor: Colors.secondary900 } : { backgroundColor: Colors.secondary500 }]}>
             <Pressable onPress={onSelect}>
                 <View style={styles.pressContainer}>
-                    <Text style={[styles.childrenText, children == "CONTINUE" ? { color: Colors.secondary500 } : { color: Colors.secondary900 }]}>{children}</Text>
+                    <Text style={[styles.childrenText, status == "ok" ? { color: Colors.secondary500 } : { color: Colors.secondary900 }]}>{children}</Text>
                 </View>
             </Pressable>
         </View>
---
+++ Components/popUps/ErrorPopUp.js
@@ -0,0 +1,79 @@
+import { Modal, StyleSheet, Text, View } from "react-native";
+import Colors from "../../Constants/colors";
+import { moderateScale, scale, verticalScale } from "../../Context/scales";
+import CustomButton from "../CustomButton";
+
+const ErrorPopUp =({visible, message, errorHandler, errorMsgHandler}) =>{
+
+    const handleClose = () =>{
+        errorMsgHandler("");
+        errorHandler(false);
+    }
+    
+    return (
+        <Modal visible={visible} animationType="fade" statusBarTranslucent={true} transparent={true}>
+            <View style={styles.outerCon}>
+                <View style={styles.container}>
+                    <View style={styles.headCon}>
+                        <Text style={styles.popHeading}>Error</Text>
+                    </View>
+                    <View style={styles.innerContainer}>
+                        <View style={styles.subContainers}>
+                            <Text>{message}</Text>
+                        </View>
+                        
+                        <View style={[styles.subContainers, styles.buttonCon]}>
+                            <CustomButton onSelect={handleClose} status="ok">OK</CustomButton>
+                        </View>
+                    </View>
+                </View>
+            </View>
+        </Modal>
+    );
+}
+
+export default ErrorPopUp;
+
+const styles = StyleSheet.create({
+    outerCon: {
+        flex: 1,
+        backgroundColor: Colors.popUpBack,
+        opacity: 0.85,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    container: {
+        // flex: 1,
+        borderRadius: scale(20),
+        // alignItems: "center",
+        backgroundColor: Colors.backgroundAll,
+        width: "75%"
+
+    },
+    headCon: {
+        paddingVertical: verticalScale(15),
+        backgroundColor: Colors.primary800,
+        borderTopLeftRadius: scale(20),
+        borderTopRightRadius: scale(20)
+    },
+    popHeading: {
+        fontSize: moderateScale(20),
+        fontWeight: "bold",
+        color: Colors.PrimaryHeading,
+        paddingLeft: scale(30)
+    },
+    innerContainer: {
+        justifyContent: "center",
+        paddingHorizontal: scale(30),
+        paddingBottom: verticalScale(20),
+
+    },
+    subContainers: {
+        paddingVertical: verticalScale(10),
+        // backgroundColor: 'red'
+    },
+    buttonCon: {
+        flexDirection: 'row',
+        justifyContent: 'center'
+    }
+});
